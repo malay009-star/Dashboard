3db2799e852f5652033a9a4afb9895ae5617ebe6
malay009-star <117511789+malay009-star@users.noreply.github.com>
2024-03-25 09:45:55 +0500

    project uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={
           <AuthLauout>
-            <Dogbookingsystem />
+            <Login />
           </AuthLauout>
         } />
         <Route path='/login' element={
